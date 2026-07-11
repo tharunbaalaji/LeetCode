0c25018986377b7539febfa40e54d3658b764aa7
Tharun Balaaji S <tharunbalaaji@gmail.com>
2026-07-11 09:26:57 +0530

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int start=-1;
+        int end=-1;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                start=i;
+                break;
+            }
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target && i!=start)
+            {
+                end=i;
+            }
+        }
+        int []result=new int[2];
+        result[0]=start;
+        result[1]=end;
+        return result;
+    }
+}
