3383112e862158598683ea1ef899b39389278b42
Tharun Balaaji S <tharunbalaaji@gmail.com>
2026-07-17 14:18:56 +0530

    LC-find-the-index-of-the-first-occurrence-in-a-string [Runtime: N/A; Memory: N/A]

--- string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,9 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+        if(haystack.length()==1)
+        {
+            return 0;
+        }
         
           for(int i=0;i<haystack.length()-needle.length();i++)
           {
