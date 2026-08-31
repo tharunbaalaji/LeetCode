323fe4a9f2d833ca3b1cd8b177933db8f1220eb0
Tharun Balaaji S <tharunbalaaji@gmail.com>
2026-08-31 12:04:46 +0530

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ bracket-sequences/valid-parentheses.java
@@ -0,0 +1,57 @@
+class Solution {
+    int size;
+    int top=-1;
+    char[]stack;
+    Solution()
+    {
+        size=100;
+        stack=new char[size];
+    }
+    void push(char a)
+    {
+        if(top==size-1)
+        {
+            return;
+        }
+        stack[++top]=a;
+    }
+    char pop()
+    {
+        if(top==-1)
+        {
+            return '\0';
+        }
+        return stack[top--];
+    }
+    boolean isEmpty()
+    {
+        if(top==-1)
+        { 
+          return true;
+        }
+        return false;
+    }
+    public boolean isValid(String s) {
+
+          for(char c:s.toCharArray())
+          {
+             if(c=='{'||c=='['||c=='(')
+             {
+                push(c);
+             }
+             else if(c=='}'||c==']'||c==')')
+             {
+                char temp=pop();
+                if((temp=='{'&&c!='}')||(temp=='['&&c!=']')||(temp=='('&&c!=')'))
+                {
+                    return false;
+                }
+              }
+          }
+          if(isEmpty())
+          {
+            return true;
+          }
+          return false;
+    }
+}
